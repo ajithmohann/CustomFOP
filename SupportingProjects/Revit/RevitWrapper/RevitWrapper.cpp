a6c40d8d8f8dbb512083a156d721ebb7e1d78d4b
ajithmohan <ajith.mohan@autodesk.com>
2016-11-04 12:46:56 +0800

    Added projects for Revit

---
+++ SupportingProjects/Revit/RevitWrapper/RevitWrapper.cpp
@@ -0,0 +1,18 @@
+// This is the main DLL file.
+
+#include "stdafx.h"
+
+#include "RevitWrapper.h"
+
+namespace RevitWrapper {
+
+	void RevitFilePropertiesManager::GetFileProperties()
+	{
+
+	}
+
+	void RevitFilePropertiesManager::UpdateFileProperties()
+	{
+
+	}
+}
---
+++ SupportingProjects/Revit/RevitWrapper/RevitWrapper.h
@@ -0,0 +1,15 @@
+// RevitWrapper.h
+
+#pragma once
+
+using namespace System;
+
+namespace RevitWrapper {
+
+	public ref class RevitFilePropertiesManager
+	{
+	public:
+		void GetFileProperties();
+		void UpdateFileProperties();
+	};
+}
